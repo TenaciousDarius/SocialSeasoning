4d2ca6259870bfe5c37918241eb41eca59908457
Darius Elphick <dariuselphick@gmail.com>
2014-05-31 21:33:11 +0100

    Added a fade back in animation on the Nerd button
    Signed-off-by: Darius Elphick <dariuselphick@gmail.com>

--- Social Seasoning/WDEViewController.m
+++ Social Seasoning/WDEViewController.m
@@ -8,8 +8,11 @@
 
 #import "WDEViewController.h"
 #import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface WDEViewController ()
+
+@property (nonatomic) BOOL isPlaying;
 
 @end
 
@@ -18,7 +21,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    self.isPlaying = false;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,7 +40,29 @@
     
 }
 
+- (void)animateButtonWithDuration:(float)seconds {
+    
+    UIColor *currentButtonColor = [self.nerdButton.backgroundColor copy];
+    self.nerdButton.backgroundColor = [UIColor blackColor];
+    self.nerdButton.alpha = 0.5;
+    
+    [UIView animateWithDuration:seconds
+                     animations:^{
+                         
+                         self.nerdButton.backgroundColor = currentButtonColor;
+                         self.nerdButton.alpha = 1.0;
+                         
+                     }];
+    
+}
+
 - (void)playSoundWithPath:(NSString *)soundPath {
+    
+    if(self.isPlaying) {
+        return;
+    } else {
+        self.isPlaying = true;
+    }
     
     NSURL *soundURL = [NSURL fileURLWithPath:soundPath];
     
@@ -46,6 +73,13 @@
                                           NULL,
                                           HandleSystemSoundCompleted,
                                           (__bridge_retained void *)self);
+    
+    AVURLAsset* audioAsset = [AVURLAsset URLAssetWithURL:soundURL
+                                                 options:nil];
+    CMTime audioDuration = audioAsset.duration;
+    float audioDurationSeconds = CMTimeGetSeconds(audioDuration);
+    [self animateButtonWithDuration:audioDurationSeconds];
+    
     AudioServicesPlaySystemSound(soundId);
     
 }
@@ -56,6 +90,8 @@ void HandleSystemSoundCompleted(SystemSoundID soundId, void *myself) {
     
     AudioServicesRemoveSystemSoundCompletion(soundId);
     AudioServicesDisposeSystemSoundID(soundId);
+    
+    [(__bridge_transfer WDEViewController *)myself setIsPlaying:false];
     
 }
 
